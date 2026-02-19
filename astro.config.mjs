90f27af9d1c32afa7c7bf28dea9ebaaca2bd91e6
astauf03 <astauf03@upenn.edu>
2026-02-19 17:55:33 -0500

    config adjustt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astauf03.github.io',
-  base: '/sensegrass_mindmap',
+  site: 'https://astauf03.github.io/sensegrass_mindmap/',
+  base: '/sensegrass_mindmap/',
   integrations: [react()],
   output: 'static'
 });
